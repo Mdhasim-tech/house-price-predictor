1476589c18fe61187589cc3f00f4677327911c12
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2025-05-13 16:49:13 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -44,7 +44,7 @@ export default function Home() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://house-price-predictor-backend.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
